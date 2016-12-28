bfe7b67197fec419bbde5bd97c59094ba83a8210
Zac Collier <zacacollier@gmail.com>
2016-12-27 18:09:55 -0600

    filtering and saving to YAML ok

--- index.js
+++ index.js
@@ -3,8 +3,27 @@ const got = require('got');
 
 const YAML = require('yamljs');
 
+function resultRemoveComments(result) {
+
+  let resultCommentsRemoved = result.map((line) => {
+    return !line.substring(0, 1).includes('#') ? line : null
+  })
+  .filter((line) => line !== null)
+  .filter(each => each !== '' )
+  .filter(each => each !== '---' )
+  .join("\r\n");
+
+  fs.writeFile('newColors.yaml', resultCommentsRemoved, (err) => {
+    if (err) throw err;
+    console.log(`Saved to ${arguments[0]}`);
+  });
+}
+
 got('https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml')
   .then(res => {
-    console.log(res.body)
+    // Remove Headers
+    let splitResult = res.body.split(/\n/)
+    resultRemoveComments(splitResult)
   })
   .catch(err => console.log(err))
+
