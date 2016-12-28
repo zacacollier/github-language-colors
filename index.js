672d381e0c4545aad1e58a846a4e92322c6a0cce
Zac Collier <zacacollier@gmail.com>
2016-12-28 16:05:03 -0600

    swanky spinner & output colors

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const YAML = require('yamljs');
 const chalk = require('chalk');
 const Spinner = require('cli-spinner').Spinner;
 
+//bind to variable to easily support other address locations
+const url = 'https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml';
+
+// interesting - when you place a '\n' into the string, it prints a bunch of
+// new lines...
+const spinner = new Spinner(chalk.yellow('Fetching... %s'));
+spinner.setSpinnerString(19);
+spinner.start();
+
 function resultRemoveComments(result) {
   let resultCommentsRemoved = result.map((line) => {
     return !line.substring(0, 1).includes('#') ? line : null
@@ -24,11 +33,14 @@ function writeToYaml(input) {
   });
 }
 
-got('https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml')
+
+got(url)
   .then(res => {
     // Remove Headers
-    let splitResult = res.body.split(/\n/)
-    resultRemoveComments(splitResult)
+    spinner.stop(!!res);
+    spinner.setSpinnerTitle(chalk.yellow('Fetched YAML, converting to JSON...'));
+    let splitResult = res.body.split(/\n/);
+    resultRemoveComments(splitResult);
   })
-  .catch(err => console.log(err))
+  .catch(err => console.log(chalk.red(err)))
 
